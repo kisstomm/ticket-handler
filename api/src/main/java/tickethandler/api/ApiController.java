7cf89e6f976b03c75344d070ebf367f44d67df05
Kiss Tamas <kisstomm@gmail.com>
2022-04-25 10:45:48 +0200

    add dummy endpoint: getEvent

--- api/src/main/java/tickethandler/api/ApiController.java
+++ api/src/main/java/tickethandler/api/ApiController.java
@@ -2,6 +2,7 @@ package tickethandler.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,12 @@ public class ApiController {
     @GetMapping("/getEvents")
     public String getEvents() {
         log.info("API - getEvents");
-        return "Hello Events";
+        return "Hello Events!";
+    }
+
+    @GetMapping("/getEvent")
+    public String getEvent(@RequestParam("eventId") Long eventId) {
+        log.info(String.format("API - getEvent: %d", eventId));
+        return String.format("Hello Event %d!", eventId);
     }
 }
