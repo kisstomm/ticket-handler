a77859452f2801ac93d28c67b69f70dc637fba8b
Kiss Tamas <kisstomm@gmail.com>
2022-04-26 12:25:40 +0200

    fix based on codeReview

--- partner/src/main/java/tickethandler/partner/repository/SeatRepository.java
+++ partner/src/main/java/tickethandler/partner/repository/SeatRepository.java
@@ -1,7 +1,9 @@
 package tickethandler.partner.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tickethandler.partner.model.Seat;
 
+@Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 }
