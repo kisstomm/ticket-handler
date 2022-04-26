f933b93b981d0e239b71cba8ba93312f1a1faf5c
Kiss Tamas <kisstomm@gmail.com>
2022-04-26 11:51:56 +0200

    create event entity and repo

---
+++ partner/src/main/java/tickethandler/partner/model/Event.java
@@ -0,0 +1,28 @@
+package tickethandler.partner.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "events")
+public class Event {
+
+    @Id
+    @Column(name = "event_id", nullable = false)
+    private Long eventId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "location", nullable = false)
+    private String location;
+
+    @Column(name = "start_timestamp", nullable = false)
+    private LocalDateTime startTimestamp;
+
+    @Column(name = "end_timestamp", nullable = false)
+    private LocalDateTime endTimestamp;
+}
---
+++ partner/src/main/java/tickethandler/partner/repository/EventRepository.java
@@ -0,0 +1,6 @@
+package tickethandler.partner.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<EventRepository, Long> {
+}
