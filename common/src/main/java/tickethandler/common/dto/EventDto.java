3a5bc5ce6f9411dfc472946d22a8ff0cbf245bb7
Kiss Tamas <kisstomm@gmail.com>
2022-04-26 13:31:29 +0200

    add mapstruct as dependency  and create common module and add as dependency

---
+++ common/src/main/java/tickethandler/common/dto/EventDto.java
@@ -0,0 +1,21 @@
+package tickethandler.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventDto {
+
+    private Long eventId;
+    private String title;
+    private String location;
+    private LocalDateTime startTimestamp;
+    private LocalDateTime endTimestamp;
+}
---
+++ common/src/main/java/tickethandler/common/dto/EventListResponseDto.java
@@ -0,0 +1,17 @@
+package tickethandler.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventListResponseDto {
+    List<EventDto> eventDtoList;
+}
