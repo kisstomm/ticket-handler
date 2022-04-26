aae1af2dd8097bd3cda64b9008a48c9a9b85606d
Kiss Tamas <kisstomm@gmail.com>
2022-04-26 11:55:23 +0200

    quick fix

--- partner/src/main/java/tickethandler/partner/repository/EventRepository.java
+++ partner/src/main/java/tickethandler/partner/repository/EventRepository.java
@@ -1,6 +1,7 @@
 package tickethandler.partner.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import tickethandler.partner.model.Event;
 
-public interface EventRepository extends JpaRepository<EventRepository, Long> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
