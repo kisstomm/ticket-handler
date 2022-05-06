be291e0369173d05327aa4ec0b30e63f148345ec
Kiss Tamas <kisstomm@gmail.com>
2022-05-06 17:44:20 +0200

    update testIsTokenValid

--- core/src/test/java/tickethandler/core/controller/CoreControllerTest.java
+++ core/src/test/java/tickethandler/core/controller/CoreControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import tickethandler.common.dto.user.UsertokenResponseDto;
+import tickethandler.common.enums.ErrorType;
 import tickethandler.core.model.Usertoken;
 import tickethandler.core.repository.UsertokenRepository;
 
@@ -41,11 +42,19 @@ public class CoreControllerTest {
     void testIsTokenValid() {
         final String tokenValid = "T0k3n";
         final String tokenInValid = "invalid token";
+        final String tokenEmpty = "";
+        final String tokenNull = null;
 
         UsertokenResponseDto usertokenResponseDto = coreController.getIsTokenValid(tokenValid);
         assertTrue(usertokenResponseDto.isSuccess());
+        assertEquals(usertokenResponseDto.getErrorType(), ErrorType.NO_ERROR);
 
         usertokenResponseDto = coreController.getIsTokenValid(tokenInValid);
         assertFalse(usertokenResponseDto.isSuccess());
+        assertEquals(usertokenResponseDto.getErrorType(), ErrorType.CORE_USER_TOKEN_INVALID);
+
+        usertokenResponseDto = coreController.getIsTokenValid(tokenEmpty);
+        assertFalse(usertokenResponseDto.isSuccess());
+        assertEquals(usertokenResponseDto.getErrorType(), ErrorType.CORE_USER_TOKEN_NULL);
     }
 }
